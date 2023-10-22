9729b493323e786fe2dfc1d07c2d9108eef0503d
ARamosA <aldoramosaro@gmail.com>
2023-10-22 12:51:36 -0500

    front react

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,23 +4,17 @@ import { Card, CardContent, CardActions, Typography, Button } from '@mui/materia
 import axios from "axios"
 function App() {
   const URL = 'http://localhost:5000/products'
-
   const [items, setItems] = useState([]);
-
-  const getInfo = async () => {
-    try {
-      const response = await axios.get(URL);
-      setItems(response)
-      return response
-    } catch (error) {
-      console.log(error)
-      return error;
-    }
-
+  const getinfo = async () => {
+    const res = await axios
+      .get(URL)
+      .catch((error) => console.log({ error }))
+    setItems(res)
+    return res
   }
 
   useEffect(() => {
-    getInfo();
+    getinfo();
   })
 
   return (
@@ -28,30 +22,15 @@ function App() {
       <p>PRODUCTOS</p>
 
       <div>
-        {items.map(item => (
-          <Card variant="outlined">
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                Word of the Day
-              </Typography>
-              <Typography variant="h5" component="div">
-                be ne vo lent
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-              </Typography>
-              <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button> Comprar </Button>
-            </CardActions>
-          </Card>
-        ))}
+        {items.map(((item, index) => (
 
+          <Card key={index}>
+            <CardContent>
+              {item.name}
+            </CardContent>
+          </Card>
+
+        )))}
       </div>
     </>
   )
